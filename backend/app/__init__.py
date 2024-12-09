38c276c33470bbc4c7043fd5087f2462f2c2ee2c
Cristian Aravena <50273105+crisstrance@users.noreply.github.com>
2024-12-09 21:10:13 +0000

    otra prueba

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,15 +2,22 @@ from flask import Flask, send_from_directory
 from flask_cors import CORS
 
 def create_app():
+    # Inicializa la app Flask y define la carpeta estática para React
     app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
-    CORS(app)  # Habilitar CORS para permitir conexiones cruzadas
-    
-    # Ruta para servir el archivo estático del frontend
+
+    # Configura CORS
+    CORS(app)
+
+    # Rutas para servir el frontend
     @app.route('/')
     def serve_frontend():
         return send_from_directory(app.static_folder, 'index.html')
 
-    # Otras rutas de tu API
+    @app.route('/<path:path>')
+    def serve_static_files(path):
+        return send_from_directory(app.static_folder, path)
+
+    # Registrar el Blueprint de la API
     from .routes import api
     app.register_blueprint(api, url_prefix='/api')
 
