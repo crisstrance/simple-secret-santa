e1cbe1e50d9152ce45b53ed2978552c280903431
Cristian Aravena <50273105+crisstrance@users.noreply.github.com>
2024-12-09 16:26:46 +0000

    pre build to render

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 @app.route("/")
 def home():
