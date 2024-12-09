43a51698e17c662487b1e9f37f9859d7d74e9b24
Cristian Aravena <50273105+crisstrance@users.noreply.github.com>
2024-12-09 19:10:30 +0000

    backend link fixed

--- backend/app.py
+++ backend/app.py
@@ -4,5 +4,9 @@ from app import create_app  # Asegúrate de importar la función create_app
 
 app = create_app()  # Usamos la factory para crear la app
 
+@app.route('/')
+def home():
+    return "Hello, World!"
+
 if __name__ == "__main__":
     app.run(debug=True)
