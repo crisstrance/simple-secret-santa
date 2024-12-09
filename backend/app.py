20c6867d03e271d51fe1af343f706d94b8e5a02c
Cristian Aravena <50273105+crisstrance@users.noreply.github.com>
2024-12-09 20:58:41 +0000

    otra prueba

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,11 @@
-import os
-from flask import Flask, send_from_directory
+from app import create_app
 
-def create_app():
-    app = Flask(__name__, static_folder='frontend/build', static_url_path='')
+# Llama a la función para crear la aplicación
+app = create_app()
 
-    # Configuramos el puerto con la variable de entorno `PORT`
-    port = os.environ.get('PORT', 5000)
-
-    # Ruta para servir la página principal de React
-    @app.route('/')
-    def serve_frontend():
-        return send_from_directory('frontend/build', 'index.html')
-
-    # Ruta para servir archivos estáticos como JS, CSS, imágenes, etc.
-    @app.route('/<path:path>')
-    def serve_static(path):
-        return send_from_directory('frontend/build', path)
-
-    # Inicia la app Flask en el puerto correcto
-    app.run(host='0.0.0.0', port=port, debug=True)
-
-    return app
+# Ejecuta la aplicación
+if __name__ == '__main__':
+    import os
+    # Obtén el puerto de las variables de entorno, usa 5000 como predeterminado
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 
 def create_app():
-    app = Flask(__name__)
-    CORS(app)  # Configuramos CORS
-    from .routes import api  # Importamos el Blueprint de las rutas
-    app.register_blueprint(api, url_prefix='/api')  # Registramos el Blueprint con el prefijo
+    app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
+    CORS(app)  # Habilitar CORS para permitir conexiones cruzadas
+    
+    # Ruta para servir el archivo estático del frontend
+    @app.route('/')
+    def serve_frontend():
+        return send_from_directory(app.static_folder, 'index.html')
+
+    # Otras rutas de tu API
+    from .routes import api
+    app.register_blueprint(api, url_prefix='/api')
 
     return app
